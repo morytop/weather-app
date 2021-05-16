e751f634611b4055ef804cc6bc4a91a9e80376d5
Piotr Moryto <piotrmoryto@gmail.com>
2021-05-16 13:34:13 +0200

    add app data and display weather to ui

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-//Select elements
 const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
+
+const weather = {};
+
+weather.temperature = {
+    unit: "celsius"
+}
+
+function displayWeather() {
+    iconElement.innerHTML = `<img src="./icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+}
