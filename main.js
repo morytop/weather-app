1951c7213b664dd39b1431e8898d9b1cefdf4db6
Piotr Moryto <piotrmoryto@gmail.com>
2021-05-16 13:51:39 +0200

    add listening when the user clicks on the temp element

--- main.js
+++ main.js
@@ -20,3 +20,18 @@ function displayWeather() {
 function celsiusToFahrenheit(temperature) {
     return (temperature * 9 / 5) + 32;
 }
+
+tempElement.addEventListener("click", function () {
+    if (weather.temperature.value === undefined) return;
+
+    if (weather.temperature.unit == "celsius") {
+        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        fahrenheit = Math.floor(fahrenheit);
+
+        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
+        weather.temperature.unit = "fahrenheit";
+    } else {
+        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+        weather.temperature.unit = "celsius"
+    }
+});
