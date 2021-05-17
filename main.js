6e02dd3bdb5573517cc395df511a3c3b0c0ce6eb
Piotr Moryto <piotrmoryto@gmail.com>
2021-05-17 10:27:50 +0200

    fixes

--- main.js
+++ main.js
@@ -21,39 +21,39 @@ if ('geolocation' in navigator) {
 }
 
 function setPosition(position) {
-    let latitude = position.coord.latitude;
-    let longitude = position.coord.longitude;
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
     getWeather(latitude, longitude);
 }
 
 function showError(error) {
     notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p>${error.message}</p>`;
+    notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
 function getWeather(latitude, longitude) {
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={key}`;
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
         .then(function (response) {
             let data = response.json();
             return data;
         })
-        .then(function (response) {
+        .then(function (data) {
             weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
             weather.iconId = data.weather[0].icon;
             weather.city = data.name;
             weather.country = data.sys.country;
         })
         .then(function () {
             displayWeather();
-        })
-
+        });
 }
 
 function displayWeather() {
-    iconElement.innerHTML = `<img src="./icons/${weather.iconId}.png"/>`;
+    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
